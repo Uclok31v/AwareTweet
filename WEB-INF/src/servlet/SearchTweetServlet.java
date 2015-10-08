e943e908454c316b1fdb0419d802196696b1721a
鵜戸西謙太郎 <a131404n@st.u-gakugei.ac.jp>
2015-10-08 16:45:55 +0900

    add

---
+++ WEB-INF/src/servlet/SearchTweetServlet.java
@@ -0,0 +1,40 @@
+package servlet;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controller.SearchTweetManager;
+import beans.User;
+
+public class SearchTweetServlet extends HttpServlet{
+
+	
+
+	public void doGet(HttpServletRequest request,HttpServletResponse response)
+		throws ServletException,IOException{
+			doPost(request,response);
+	}
+
+	public void doPost(HttpServletRequest request,HttpServletResponse response)
+		throws ServletException,IOException{
+
+		request.setCharacterEncoding("UTF-8");
+
+		String searchWord = request.getParameter("search_word");; 
+
+		SearchTweetManager getUserTweetManager = new SearchTweetManager();
+		
+		ArrayList list = getUserTweetManager.selectTweetByComment(searchWord);
+
+        request.setAttribute("tweetList",list);
+		getServletContext().getRequestDispatcher("/jsp/hazelab/viewusertweet.jsp").forward(request, response);
+
+		}
+	}
