5f64d69d762837d46c9a2d5ffe8241e994280cdc
shu- <m153304w@st.u-gakugei.ac.jp>
2015-12-01 18:19:54 +0900

    udonisi

---
+++ WEB-INF/src/utility/DriverAccessor.java
@@ -0,0 +1,40 @@
+package utility;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DriverAccessor {
+
+	private final static String DRIVER_URL="jdbc:mysql://localhost:3306/awaretweet?useUnicode=true&characterEncoding=Windows-31J";
+
+
+	private final static String DRIVER_NAME="com.mysql.jdbc.Driver";
+
+
+	private final static String USER_NAME="tweet";
+
+
+	private final static String PASSWORD="tweet";
+
+
+	public Connection createConnection(){
+	try{
+		Class.forName(DRIVER_NAME);
+		Connection con=DriverManager.getConnection(DRIVER_URL,USER_NAME,PASSWORD);
+		return con;
+		}catch(ClassNotFoundException e){
+			System.out.println("Can't Find JDBC Driver.\n");
+			}catch(SQLException e){
+				 System.out.println("Connection Error.\n");
+				}
+				return null;
+		}
+
+	public void closeConnection(Connection con){
+		try{
+			con.close();
+		}catch(Exception ex){}
+	}
+}
---
+++ WEB-INF/src/utility/DriverAccessor_Hazelab.java
@@ -0,0 +1,40 @@
+package utility;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DriverAccessor_Hazelab {
+
+	private final static String DRIVER_URL="jdbc:mysql://localhost:3306/lab_members?useUnicode=true&characterEncoding=Windows-31J";
+
+
+	private final static String DRIVER_NAME="com.mysql.jdbc.Driver";
+
+
+	private final static String USER_NAME="hazelab";
+
+
+	private final static String PASSWORD="mallow";
+
+
+	public Connection createConnection(){
+	try{
+		Class.forName(DRIVER_NAME);
+		Connection con=DriverManager.getConnection(DRIVER_URL,USER_NAME,PASSWORD);
+		return con;
+		}catch(ClassNotFoundException e){
+			System.out.println("Can't Find JDBC Driver.\n");
+			}catch(SQLException e){
+				 System.out.println("Connection Error.\n");
+				}
+				return null;
+		}
+
+	public void closeConnection(Connection con){
+		try{
+			con.close();
+		}catch(Exception ex){}
+	}
+}
