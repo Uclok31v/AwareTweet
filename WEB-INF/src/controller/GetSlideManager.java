4de93e88aed15f23a2024101b5f4548effd72a9f
shu- <m153304w@st.u-gakugei.ac.jp>
2015-12-08 02:27:50 +0900

    reverse

--- WEB-INF/src/controller/GetSlideManager.java
+++ WEB-INF/src/controller/GetSlideManager.java
@@ -7,9 +7,9 @@ import utility.HostPathComponent;
 public class GetSlideManager {
 
 	public GetSlideManager(){
-		
+
 	}
-	
+
 	public void checkDirectory(String user_id) {
 		// TODO Auto-generated method stub
 		//ホストによって異なるパス
@@ -17,7 +17,7 @@ public class GetSlideManager {
 		String hostPath = createHostPath.createHostPath();
 		File file = new File(hostPath +"AwareTweet/slide/"+user_id);
 		if(file.exists()){
-			
+
 		}
 		else{
 			file.mkdir();
@@ -28,10 +28,10 @@ public class GetSlideManager {
 		// TODO Auto-generated method stub
 		HostPathComponent createHostPath = new HostPathComponent();
 		String hostPath = createHostPath.createHostPath();
-		
+
 		//探索するパス
 		String basePath = hostPath + "AwareTweet/slide/" + user_id;
-		
+
 		File dir = new File(basePath);
 		File[] files = dir.listFiles();
 		return files;
